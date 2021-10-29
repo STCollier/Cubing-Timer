74f3ef559de97c1c6fb6cada1ad2789de24aea57
STCollier <81338469+STCollier@users.noreply.github.com>
2021-10-29 12:42:26 -0400

    Update script.js
    Added penalty alert

--- script.js
+++ script.js
@@ -8,6 +8,17 @@ startTime = Date.now();
   interval = setInterval(function() {
       elapsedTime = Date.now() - startTime;
       document.getElementById("timer").innerHTML = (elapsedTime / 1000).toFixed(2);
+      
+      if (elapsedTime/1000 >= 8) {
+        document.getElementById("alert").innerHTML = "8 Seconds"
+      } if (elapsedTime/1000 >= 12) {
+        document.getElementById("alert").innerHTML = "12 Seconds"
+      } if (elapsedTime/1000 >= 15) {
+        document.getElementById("alert").innerHTML = "+2"
+      } if (elapsedTime/1000 >= 17) {
+        document.getElementById("alert").innerHTML = "DNF"
+      }
+
   }, 10);
   document.getElementById("start").innerHTML = "Start"
 
